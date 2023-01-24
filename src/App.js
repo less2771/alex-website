ce00ca3110bb178ec4f3e089363d9e7d30ff1e4c
less2771 <74804307+less2771@users.noreply.github.com>
2023-01-24 20:47:41 +0800

    v1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Home from './homePage';
+import Login from './testLogin';
+import NotFound from './NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Link to="/">Home Page</Link>
+      <Link to="/testLogin">Test Login Page</Link>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/testLogin" element={<Login />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+
+    </BrowserRouter>
+
   );
 }
 
